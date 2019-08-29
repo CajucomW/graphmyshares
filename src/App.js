4cc815f499b5b3dcec3f7e1d640d98b0616b1b99
Wence Cajucom <cajucom.w@gmail.com>
2019-08-28 18:30:41 -0700

    Michael added some suggestions

--- src/App.js
+++ src/App.js
@@ -3,100 +3,91 @@ import React, { Component } from 'react';
 import './site.css';
 
 class App extends Component {
-    // Most common lifecycle method: componentDidMount
+  state = {
+    stockList: [
+      {
+        symbol: "",
+        price: 0,
+      },
+    ],
+  };
+
+// I will need some sort of <form> or <input> field to narrow down the selection
+// Values from Form/Input will need to search the API and retrieve
+// Retrieved values need to modify the State, which then modifies Render
+
+  // barAttribute = () => {
+  //   console.log('***Bar Attribute?');
+  //   this.setState({
+  //     symbol: this.state.symbol,
+  //     price: this.state.price,
+  //   });
+  // }
+
   componentDidMount = () => {
-    // Gets run right away. Typically used to go to APIs to get data to
-    // display.  In this case, fetching a repo list from GitHub.
     fetch("https://financialmodelingprep.com/api/v3/stock/real-time-price")
       .then(response => response.json())
       .then(data => {
         console.log('Did component mount?', data);
-  //       this.setState({
-  //           results: data,
-  //           isLoading: false,
+// make a newList Michael's suggestion
+// specify whick symbols I want from the stockList Michael's suggestion
+// push the items in the newList
+        let newList = [];
+        for (let item of data.stockList) {
+          if (item.symbol === "GHM"
+                || item.symbol === "ACP") {
+              newList.push(item);
+          }
+        }
+// make newList become stockList Michael's suggestion
+        this.setState({
+          stockList: newList,
         });
-  //     });
+      });
   }
   render() {
     return (
-      // <div className="App">
+      <div className="App">
+      {/* <div> */}
+        <h1>Title Goes Here</h1>
         <div className="MainContentBorder">
-          {/* <h1 class="MainTitle">Stock Market Prices</h1> */}
-            <div className="BarsInGraph BarOne" 
-            onClick="console.log('Clicked on McDondald');
-            alert('A share of McDonalds Corp stock is worth: $219.71')">
-                <h2 className="BarNamePosition">McDonald's</h2>
+            <div className="BarsInGraph">
+              {
+                this.state.stockList.map(data => (
+                  <div className="BarsInGraph" style={{height: data.price + "%"}}>
+                    <h2 className="BarNamePosition">{data.symbol}</h2> 
+                    </div>
+                ))
+              }
             </div>
-            <div className="BarsInGraph BarTwo" 
-            onClick="console.log('Clicked on Wendys');
-            alert('A share of Wendys stock is worth: $19.65')">
-                <h2 className="BarNamePosition">Wendy's</h2>
-            </div>
-            <div className="BarsInGraph BarThree" 
-            onClick="console.log('Clicked on Chipotle');
-            alert('A share of Chipotle stock is worth: $817')">
-                <h2 className="BarNamePosition">Chipotle</h2>
-            </div>
-            <div className="BarsInGraph BarFour" 
-            onClick="console.log('Clicked on Pepsi');
-            alert('A share of PepsiCo stock is worth: $129.94')">
-                <h2 className="BarNamePosition">Pepsi</h2>
-            </div>
-            <div className="BarsInGraph BarFive" 
-            onClick="console.log('Clicked on Coke');
-            alert('A share of Coca-Cola stock is worth: $351.67')">
-                <h2 className="BarNamePosition">Coca-Cola</h2>
-            </div>
-          </div>
-      // </div>
+        </div>
+      </div>
     );
   }
-}  /*
-  doFetch will do the fetch to the API based on state, updating the state with
-  the books retrieved.
-  */
-  // doFetch = () => {
-  //   const url = this.getURL();
-  //   console.log('making query to ', url);
-  //   this.setState({isLoading: true});
-  //   fetch(url)
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       // Calculate the total pages, based on the page size and the number of
-  //       // results
-  //       const totalPages = Math.ceil(data.numFound / PAGE_SIZE);
-  //       console.log('data', data);
-
-  //       // Set the state of the new information, notably the total number of
-  //       // pages, and the array of data we got back
-  //       this.setState({
-  //         totalPages: totalPages,
-  //         books: data.docs,
-  //         isLoading: false,
-  //       });
-  //     });
-  // }
-// ** Example of Fetching Data from API **
-    // function doFetch() {
-    //     fetch('https://ghibliapi.herokuapp.com/films')
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             console.log(data);
-    //             state.data = data;
-    //         });
-    // }
-
-    // fetch(`https://api.github.com/users/${this.state.username}/repos`)
-    //   .then(response => response.json())
-    //   .then(data => {
-
-    //     // Check for errors:
-    //     if (data.message ===  "Not Found") {
-    //       this.setState({
-    //           results: [],
-    //           isLoading: false,
-    //       });
-    //       return;
-    //     }
+}
 
 export default App;
+
+
+            // <div className="BarsInGraph BarTwo"
+            // onClick="console.log('Clicked on Wendys');
+            // alert('A share of Wendys stock is worth: $19.65')">
+            //    <h2 className="BarNamePosition">Wendy's</h2>
+            // </div>
+            // <div className="BarsInGraph BarThree"
+              // onClick="console.log('Clicked on Chipotle');
+              // alert('A share of Chipotle stock is worth: $817')">
+              //     <h2 className="BarNamePosition">Chipotle</h2>
+            // </div>
+            // <div className="BarsInGraph BarFour" 
+              // onClick="console.log('Clicked on Pepsi');
+              // alert('A share of PepsiCo stock is worth: $129.94')">
+              //     <h2 className="BarNamePosition">Pepsi</h2>
+            // </div>
+            // <div className="BarsInGraph BarFive" 
+              // onClick="console.log('Clicked on Coke');
+              // alert('A share of Coca-Cola stock is worth: $351.67')">
+              //     <h2 className="BarNamePosition">Coca-Cola</h2>
+            // </div>
+        // </div>
+      // </div>
