fba65e64139df26d2a68fb982bf207b40ba5d15d
Wence Cajucom <cajucom.w@gmail.com>
2019-08-29 21:17:59 -0700

    Maddy helped with submit button, input field, and modifying the bars.

--- src/App.js
+++ src/App.js
@@ -4,90 +4,88 @@ import './site.css';
 
 class App extends Component {
   state = {
+    searchTerm: '',
     stockList: [
       {
         symbol: "",
-        price: 0,
+        price: '',
       },
     ],
   };
 
-// I will need some sort of <form> or <input> field to narrow down the selection
-// Values from Form/Input will need to search the API and retrieve
-// Retrieved values need to modify the State, which then modifies Render
-
-  // barAttribute = () => {
-  //   console.log('***Bar Attribute?');
-  //   this.setState({
-  //     symbol: this.state.symbol,
-  //     price: this.state.price,
-  //   });
-  // }
-
-  componentDidMount = () => {
-    fetch("https://financialmodelingprep.com/api/v3/stock/real-time-price")
-      .then(response => response.json())
-      .then(data => {
-        console.log('Did component mount?', data);
+  // componentDidMount = () => {
+  //   fetch("https://financialmodelingprep.com/api/v3/stock/real-time-price")
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log('Did component mount?', data);
 // make a newList Michael's suggestion
 // specify whick symbols I want from the stockList Michael's suggestion
 // push the items in the newList
-        let newList = [];
-        for (let item of data.stockList) {
-          if (item.symbol === "GHM"
-                || item.symbol === "ACP") {
-              newList.push(item);
-          }
-        }
+        // let newList = [];
+        // for (let item of data.stockList) {
+        //   if (item.symbol === "CMG"
+        //         || item.symbol === "MCD"
+        //         || item.symbol === "WEN"
+        //         || item.symbol === "COKE"
+        //         || item.symbol === "PEP") {
+        //       newList.push(item);
+        //   }
+        // }
 // make newList become stockList Michael's suggestion
-        this.setState({
-          stockList: newList,
-        });
-      });
+  //       this.setState({
+  //         stockList: newList,
+  //       });
+  //       console.log('this.state.stockList', this.state.stockList);
+  //     });
+  // }
+
+  newOnSearchTermChange = (ev) => {
+    let value = ev.target.value;
+    console.log('is this working?');
+    this.setState({
+       searchTerm: value,
+    });
+    console.log('is setState working?', this.state.searchTerm);
   }
+
+  onSubmit = () => {
+    let listPlusInput = this.state.stockList;
+    console.log('is the button working?', this.state.searchTerm);
+    fetch('https://financialmodelingprep.com/api/v3/stock/real-time-price/' + this.state.searchTerm)
+        .then(response => response.json())
+        .then(data => {
+          listPlusInput.push(data)
+          console.log('this is the data', data);
+          this.setState({
+            stockList: listPlusInput,
+          });
+        })
+  }
+
   render() {
     return (
       <div className="App">
-      {/* <div> */}
         <h1>Title Goes Here</h1>
         <div className="MainContentBorder">
-            <div className="BarsInGraph">
-              {
-                this.state.stockList.map(data => (
-                  <div className="BarsInGraph" style={{height: data.price + "%"}}>
-                    <h2 className="BarNamePosition">{data.symbol}</h2> 
-                    </div>
+          {
+            this.state.stockList.map(data => (
+              <div className="BarsInGraph" style={{height: (data.price / 30) + "%"}}>
+                <h2 className="BarNamePosition">{data.symbol}<br /> {data.price}</h2>
+                </div>
                 ))
-              }
-            </div>
+            }
         </div>
+        <input
+            placeholder="Enter Stock Symbol Here"
+            value={this.state.searchTerm}
+            onChange={this.newOnSearchTermChange} />
+
+        <button onClick={() => this.onSubmit()}>Submit</button>
+
+          
       </div>
     );
   }
 }
 
 export default App;
-
-
-            // <div className="BarsInGraph BarTwo"
-            // onClick="console.log('Clicked on Wendys');
-            // alert('A share of Wendys stock is worth: $19.65')">
-            //    <h2 className="BarNamePosition">Wendy's</h2>
-            // </div>
-            // <div className="BarsInGraph BarThree"
-              // onClick="console.log('Clicked on Chipotle');
-              // alert('A share of Chipotle stock is worth: $817')">
-              //     <h2 className="BarNamePosition">Chipotle</h2>
-            // </div>
-            // <div className="BarsInGraph BarFour" 
-              // onClick="console.log('Clicked on Pepsi');
-              // alert('A share of PepsiCo stock is worth: $129.94')">
-              //     <h2 className="BarNamePosition">Pepsi</h2>
-            // </div>
-            // <div className="BarsInGraph BarFive" 
-              // onClick="console.log('Clicked on Coke');
-              // alert('A share of Coca-Cola stock is worth: $351.67')">
-              //     <h2 className="BarNamePosition">Coca-Cola</h2>
-            // </div>
-        // </div>
-      // </div>
